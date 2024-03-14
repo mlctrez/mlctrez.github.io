92e2e0b9d5d697d76db423ef2d05a94de3e742b6
mlctrez <mlctrez@gmail.com>
2024-03-13 21:51:40 -0500

    use unexported boolean field for update available

--- goapp/compo/version.go
+++ goapp/compo/version.go
@@ -10,33 +10,32 @@ var _ app.Mounter = (*Version)(nil)
 
 type Version struct {
 	app.Compo
-	UpdateAvailable string
+	updateAvailable bool
 }
 
 func (v *Version) OnMount(ctx app.Context) {
-	app.Log("OnMount", fmt.Sprintf("%v %q", &v, v.UpdateAvailable))
+	app.Log("OnMount", fmt.Sprintf("%v %v", &v, v.updateAvailable))
+	ctx.Async(ctx.TryUpdateApp)
 }
 
 func (v *Version) OnAppUpdate(ctx app.Context) {
-	app.Log("OnAppUpdate 1", fmt.Sprintf("%v %q", &v, v.UpdateAvailable))
+	app.Log("OnAppUpdate 1", fmt.Sprintf("%v %v", &v, v.updateAvailable))
 	if ctx.AppUpdateAvailable() {
-		v.UpdateAvailable = "yes"
-		app.Log("OnAppUpdate 2", fmt.Sprintf("%v %q", &v, v.UpdateAvailable))
+		v.updateAvailable = true
+		app.Log("OnAppUpdate 2", fmt.Sprintf("%v %v", &v, v.updateAvailable))
 	}
 }
 
 func (v *Version) Render() app.UI {
-	app.Log("Render", fmt.Sprintf("%v %q", &v, v.UpdateAvailable))
-	if v.UpdateAvailable == "yes" {
-		return app.Div().Class("version").Text("Update Available").
-			OnClick(func(ctx app.Context, e app.Event) {
-				ctx.Reload()
-			})
-	}
-	return app.Div().Class("version").Text(app.Getenv("GOAPP_VERSION")).
-		OnClick(func(ctx app.Context, e app.Event) {
-			ctx.Dispatch(func(context app.Context) {
-				context.TryUpdateApp()
-			})
+	app.Log("Render", fmt.Sprintf("%v %q", &v, v.updateAvailable))
+	if v.updateAvailable {
+		return app.Div().Class("version").Text("Update Available").OnClick(func(ctx app.Context, e app.Event) {
+			ctx.Reload()
 		})
+	}
+	return app.Div().Class("version").Text(app.Getenv("GOAPP_VERSION")).OnClick(func(ctx app.Context, e app.Event) {
+		ctx.Dispatch(func(context app.Context) {
+			context.TryUpdateApp()
+		})
+	})
 }
